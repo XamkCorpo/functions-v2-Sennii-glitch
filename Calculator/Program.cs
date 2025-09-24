511cdb3caca71e9cce952cac5499e37aafb576ac
Omistaja <Omistaja@DESKTOP-T4KILA5>
2025-09-24 14:50:34 +0300

    CalculateOperation, GeOperationSymbol...

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -70,13 +70,12 @@
 
 
         }
-        static double KysyEnsimmainenLuku()
+        static double KysyLuku()
         {
             double num1;
 
             while (true)
             {
-                Console.Write("Syötä ensimmäinen luku: ");
                 string lukuSyote = Console.ReadLine();
                 if (double.TryParse(lukuSyote, out num1))
                 {
@@ -92,26 +91,7 @@
 
         }
 
-        static double KysyToinenLuku()
-        {
-            double num2;
 
-            while (true)
-            {
-                Console.Write("Syötä toinen luku: ");
-                string lukuSyote = Console.ReadLine();
-                if (double.TryParse(lukuSyote, out num2))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Anna oikeanlainen luku.");
-                }
-
-            }
-            return num2;
-        }
 
 
         static double Add(double a, double b)
@@ -130,9 +110,54 @@
         {
             return a / b;
         }
+        static double CalculateOperation(int operation, double num1, double num2)
+        {
+            double result = 0;
+
+            if (operation == 1)
+            {
+                result = Add(num1, num2);
+            }
+            else if (operation == 2)
+            {
+                result = Subtract(num1, num2);
+
+            }
+            else if (operation == 3)
+            {
+                result = Multiply(num1, num2);
+            }
+            else if (operation == 4)
+            {
+                result = Divide(num1, num2);
+            }
+
+            return result;
+        }
+        private static string GetOperationSymbol(int valinta)
+        {
+            string symbol = "";
+            if (valinta == 1)
+            {
+                symbol = "+";
+            }
+            else if (valinta == 2)
+            {
+                symbol = "-";
+            }
+            else if (valinta == 3)
+            {
+                symbol = "*";
+            }
+            else if (valinta == 4)
+            {
+                symbol = "/";
+            }
+            return symbol;
+        }
         static string TulostaTulos()
         {
-            Console.WriteLine("Tulos: ");
+            Console.WriteLine($"Tulos: {num1} {GetOperationSymbol} {num2} = {result} ");
             return "";
         }
 
@@ -140,8 +165,12 @@
         static void Main(string[] args)
         {
             int valinta = ValitseLaskutoimitus();
-            double num1 = KysyEnsimmainenLuku();
-            double num2 = KysyToinenLuku();
+
+            Console.WriteLine("Anna ensimmäinen luku");
+            double num1 = KysyLuku();
+
+            Console.WriteLine("Anna toinen luku");
+            double num2 = KysyLuku();
 
 
             //tee funktio joka kysyy numeron 1, muista validoita että syöte on numero. Palauta doulbe tyyppinen numero
@@ -152,8 +181,19 @@
             //Tee funktio joka suorittaa operaation
             //Funktio ottaa syötteenä operaatio sekä numerot joille operaatio tehdään
 
-            //double result = CalculateOperation(valinta, num1, num2)
+            double result = CalculateOperation(valinta, num1, num2);
+
+            //Muuta valinta numero symbooliksi, tee tälle oma funktio
+            //Eli jos numero on 1 niin silloin tiedät, että symboli on +
+
+            string symbol = GetOperationSymbol(valinta);
+
+            //Tulosta tulos
         }
+
+
+
+
 
 
 
